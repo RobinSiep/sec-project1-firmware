f3d26f2bab85ece0183200d0ba58b05e2564b83e
Robin Siep <RobinSiep1@gmail.com>
2016-10-10 15:45:16 +0200

    solved memory problem

--- led.lua
+++ led.lua
@@ -6,37 +6,42 @@ local led = {}
 
 local pin = config.gpio
 
-led.on = false
+led.index = 0
+led.pattern = {}
+led.patternSize = 0;
 
-led.control = function(state)
-    gpio.write(pin, state)
-end
+tmr.register(2, 500, tmr.ALARM_SEMI, function() led.vibrate(led.pattern) end)
 
 led.vibrate = function(pattern)
-    for i, state in ipairs(pattern) do
-        if state == 0 then
-            gpio.write(pin, gpio.HIGH)
-        else
-            gpio.write(pin, gpio.LOW)
-        end
+    led.patternSize = table.getn(led.pattern)
+    state = led.pattern[led.index]
 
-        tmr.delay(100000)
+    if state == 0 then
+        gpio.write(pin, gpio.HIGH)
+    else
+        gpio.write(pin, gpio.LOW)
     end
-
-    if led.on == true then
-        led.vibrate(pattern)
+    if led.index < led.patternSize then
+        led.index = led.index + 1
+    else
+        led.index = 0
     end
+    tmr.start(2)
 end
 
-led.init = function(on, pattern)
+led.init = function()
     gpio.mode(pin, gpio.OUTPUT)
-
-    led.on = on
-    if on == true then
-        led.vibrate(pattern)
-    end
+    gpio.write(pin, gpio.HIGH)
 end
 
-led.init(true, {0,0,0,1,1,0,1,1})
+led.init()
+
+led.on = function()
+    tmr.start(2)
+end
+
+led.off = function()
+    tmr.stop(2)
+end
 
 return led
