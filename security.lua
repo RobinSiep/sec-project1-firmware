441c1869a40f8161581bb5677f1ed1130a82a99a
Robin Siep <RobinSiep1@gmail.com>
2016-10-10 10:47:24 +0200

    some checks

--- security.lua
+++ security.lua
@@ -2,12 +2,12 @@ local security = {}
 
 security.syncCryptoDecrypt = function(message)
     -- uses 128 bit AES
-    return crypto.decrypt("AES-CBC", "secretkey", message)
+    return crypto.decrypt("AES-CBC", "1234567890abcdef", message)
 end
 
 security.syncCryptoEncrypt = function(message)
     -- uses 128 bit AES
-    return crypto.encrypt("AES-CBC", "secretkey", message)
+    return crypto.encrypt("AES-CBC", "Yx9SqhHca9aeuD+hqCNjAA==", message)
 end
 
 security.verifyHMAC = function(hmac, message)
@@ -17,5 +17,8 @@ security.verifyHMAC = function(hmac, message)
     end
     return false
 end
+
+print(crypto.hmac("sha256", "Hash me please", "Yx9SqhHca9aeuD+hqCNjAA=="))
+print(node.heap())
 
 return security
