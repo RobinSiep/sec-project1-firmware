a75f8c5d24d1543f8352fabde77edb2a3877556e
Robin Siep <RobinSiep1@gmail.com>
2016-10-10 10:43:13 +0200

    led controls

--- led.lua
+++ led.lua
@@ -3,19 +3,35 @@ local led = {}
 local pin = 4
 local value = gpio.LOW
 
-led.toggle = function ()
-    if value == gpio.LOW then
-        value = gpio.HIGH
-    else
-        value = gpio.LOW
-    end
+led.on = false
 
+led.control = function(state)
+    value = state
     gpio.write(pin, value)
 end
 
-led.init = function()
-    gpio.mode(pin, gpio.OUTPUT)
-    gpio.write(pin, value)
+led.vibrate = function(pattern)
+    for i, state in ipairs(pattern) do
+        print("loop")
+        if state == 0 then
+            gpio.write(pin, gpio.HIGH)
+        else
+            gpio.write(pin, gpio.LOW)
+        end
+
+        tmr.delay(100000)
+    end
+
+    if led.on == true then
+        led.vibrate(pattern)
+    end
+end
+
+led.init = function(on, pattern)
+    led.on = on
+    if on == true then
+        led.vibrate(pattern)
+    end
 end
 
 return led
