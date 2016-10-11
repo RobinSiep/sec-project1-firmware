96d474168c342c92683a0f75f3cc6568e5fd08f0
Robin Siep <RobinSiep1@gmail.com>
2016-10-11 15:24:35 +0200

    modified html to include linking code

--- main.lua
+++ main.lua
@@ -1,14 +1,11 @@
-package.loaded.server = nil
 package.loaded.led = nil
 package.loaded.wsclient = nil
 package.loaded.setup = nil
-server = require "server"
 led = require "led"
 wsclient = require "wsclient"
 setup = require "setup"
 led.init()
---server.setupWifi()
 setup.wifi()
-wsclient.setup()
+--wsclient.setup()
 
 
--- setup.lua
+++ setup.lua
@@ -1,21 +1,33 @@
+package.loaded.config = nil
+
+config = require "config"
+
 local setup = {}
 
+cfg = {}
+
+cfg.pwd=config.wpapwd
+cfg.auth=AUTH_WPA2_PSK
+cfg.ssid=config.SSID
+cfg.channel=6
+cfg.hidden=0
+cfg.max=2
+cfg.beacon=100
+
 setup.wifi = function()
-    print("setup")
+    wifi.setmode(wifi.STATIONAP)
+    wifi.setphymode(wifi.PHYMODE_N)
+    wifi.ap.config(cfg)
+    enduser_setup.manual(true)
     enduser_setup.start(
-        function()
-            print("Connected")
-        end
+            function()
+                print("Connected", wifi.sta.getip())
+                enduser_setup.stop()
+            end,
+            function(err, str)
+                print(err, str)
+            end
     )
 end
 
-enduser_setup.start(
-        function()
-            print("Connected")
-        end,
-        function(err, str)
-            print(err, str)
-        end
-)
-    
 return setup
